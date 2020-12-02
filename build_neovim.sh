b97e85d603198a300342302d95d2342702c01372
Michael Maloney <tac@tac-tics.net>
2020-12-02 13:08:57 -0700

    vim: Add build_neovim.sh for building a known-working version of neovim.

---
+++ build_neovim.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -ex
+
+neovim_dir="$PROJECTS/neovim"
+neovim_commit="0a95549d66df63c06d775fcc329f7b63cbb46b2f"
+# neovim_commit="v0.4.4"
+skip_fetch=1
+prefix="$HOME/.local/neovim"
+
+if [ ! -d $neovim_dir ]
+then
+    git clone https://github.com/neovim/neovim.git $neovim_dir
+else
+    echo "$neovim_dir already exists, skipping cloning..."
+fi
+
+cd $neovim_dir
+
+if [[ "$skip_fetch" == "1" ]]
+then
+    echo "Skipping fetch..."
+else
+    git fetch
+fi
+
+git checkout $neovim_commit
+
+# make clean
+if [ ! -d build/ ]
+then
+    mkdir build/
+fi
+
+make deps
+cd build/
+pwd
+cmake \
+    -DCMAKE_INSTALL_PREFIX="$prefix" \
+    -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+    ..
+
+make
+make install
