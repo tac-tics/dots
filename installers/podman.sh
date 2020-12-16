e05cd85108f6346b8e831618cf7f05361b0c2f19
Michael Maloney <tac@tac-tics.net>
2020-12-16 02:14:58 -0700

    installers: Add podman.sh.

---
+++ installers/podman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+source /etc/os-release
+sudo sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install podman buildah skopeo
