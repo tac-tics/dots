ecc4498d04bbd28d5f0410c34cbbde38ef332637
Michael Maloney <tac@tac-tics.net>
2021-01-10 02:15:54 -0700

    installers: Fix issue with go installer.

--- installers/go.sh
+++ installers/go.sh
@@ -2,5 +2,5 @@
 
 # See: https://golang.org/doc/install
 
-curl -LO https://golang.org/dl/go1.15.6.linux-amd64.tar.gz
+curl -L https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -o /tmp/go.tar.gz
 tar xvfz /tmp/go.tar.gz -C $HOME/.local/
