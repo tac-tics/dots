820cc919e09a447956c7d39077b0ba71adf1eeb6
Michael Maloney <tac@tac-tics.net>
2021-01-16 06:46:48 +0000

    installers: Add qemu installer.

---
+++ installers/qemu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+git clone https://github.com/qemu/qemu
+cd qemu/
+mkdir build
+cd build/
+
+sudo apt install -y \
+    ninja-build \
+    libglib2.0-dev \
+    libpixman-1-dev \
+
+../configure --target-list=riscv64-softmmu --prefix=$HOME/.local/qemu
+make -j6
+make install
