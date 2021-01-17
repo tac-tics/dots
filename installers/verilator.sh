1957c625fc0e5e624833cf566839afa6806c82f1
Michael Maloney <tac@tac-tics.net>
2021-01-17 14:26:59 -0700

    installers: Add verilator.sh.

---
+++ installers/verilator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# commit="b7fec513cf2aea8cc30c5d2042f0642e7a253f54"
+commit="stable"
+
+prefix="$HOME/.local/verilator"
+
+git clone https://github.com/verilator/verilator
+cd verilator
+git checkout $commit
+autoconf
+./configure --prefix=$prefix
+make
+make install
