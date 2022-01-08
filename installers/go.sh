9cd185bc02a2487b8002e2afb02b57fe602aab73
Michael Maloney <tac@tac-tics.net>
2022-01-08 01:48:41 +0000

    installer: Bump go version.

--- installers/go.sh
+++ installers/go.sh
@@ -2,5 +2,5 @@
 
 # See: https://golang.org/doc/install
 
-curl -L https://golang.org/dl/go1.15.6.linux-amd64.tar.gz -o /tmp/go.tar.gz
+curl -L https://golang.org/dl/go1.17.5.linux-amd64.tar.gz -o /tmp/go.tar.gz
 tar xvfz /tmp/go.tar.gz -C $HOME/.local/
