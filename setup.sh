acdf2e7187ac8e3ca51da8d27a478f865efad000
Michael Maloney <tac@tac-tics.net>
2022-08-23 00:23:38 -0700

    vim: Download packer if it doesn't exist in setup.py.

--- setup.sh
+++ setup.sh
@@ -31,3 +31,8 @@ cp -r $STATIC/ipython/* $HOME/.ipython
 
 mkdir -p $HOME/.local
 cp -r $STATIC/local/* $HOME/.local
+
+if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]
+then
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+fi
