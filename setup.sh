56f0a67845042f6f0b81eec15f8f1d1fbdde35ad
Michael Maloney <tac@tac-tics.net>
2024-06-30 17:12:49 +0000

    setup: Change most things to softlinks.

--- setup.sh
+++ setup.sh
@@ -2,36 +2,38 @@
 
 set -e
 
-STATIC='static'
+function link() {
+    src=$1
+    dst=$2
+    if [ ! -e "$dst" ]; then
+        ln -s "$src" "$dst"
+    fi
+}
+
+STATIC=$(realpath 'static')
 
 # bash
 mkdir -p $HOME/.bash
-mkdir -p $HOME/.bash/completion.d
-cp -r $STATIC/bash/* $HOME/.bash/
+link $STATIC/bash/* $HOME/.bash/
 cp $STATIC/bashrc $HOME/.bashrc
 cp $STATIC/profile $HOME/.profile
 
 # git
-cp $STATIC/gitconfig $HOME/.gitconfig
-cp $STATIC/gitignore $HOME/.gitignore
+link $STATIC/gitconfig $HOME/.gitconfig
+link $STATIC/gitignore $HOME/.gitignore
 
 # command line completion
-cp $STATIC/inputrc $HOME/.inputrc
+link $STATIC/inputrc $HOME/.inputrc
 
-cp $STATIC/tmux.conf $HOME/.tmux.conf
+link $STATIC/tmux.conf $HOME/.tmux.conf
 
 mkdir -p $HOME/.config
-cp -r $STATIC/config/* $HOME/.config
+for f in $(ls -1 $STATIC/config); do
+    link "$STATIC/config/$f" "$HOME/.config/$f"
+done
 
 mkdir -p $HOME/.docker
-cp -r $STATIC/docker/* $HOME/.docker
-
-# podman
-mkdir -p $HOME/.config/containers
-cp -r $STATIC/config/containers/* $HOME/.config/containers
+link $STATIC/docker/* $HOME/.docker
 
 mkdir -p $HOME/.ipython
-cp -r $STATIC/ipython/* $HOME/.ipython
-
-mkdir -p $HOME/.local
-cp -r $STATIC/local/* $HOME/.local
+link $STATIC/ipython/* $HOME/.ipython
