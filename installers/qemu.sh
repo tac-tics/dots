00b624a93af263bb10af695c63e71f9fa44546e3
Michael Maloney <tac@tac-tics.net>
2024-07-10 09:12:46 -0700

    installers: Bump qemu.

--- installers/qemu.sh
+++ installers/qemu.sh
@@ -2,17 +2,17 @@
 
 set -ex
 
-git clone https://github.com/qemu/qemu
-cd qemu/
-git checkout v6.1.0
-mkdir build
+# git clone https://github.com/qemu/qemu $PROJECTS/qemu
+cd $PROJECTS/qemu/
+git checkout v9.0.1
+mkdir -p build
 cd build/
-
-sudo apt install -y \
-    ninja-build \
-    libglib2.0-dev \
-    libpixman-1-dev \
-
+#
+#sudo apt install -y \
+#    ninja-build \
+#    libglib2.0-dev \
+#    libpixman-1-dev \
+#
 ../configure --target-list=riscv64-softmmu --prefix=$HOME/.local/qemu
 make -j6
 make install
